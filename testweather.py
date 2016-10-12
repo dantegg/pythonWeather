43e4339f6f3890fe3a94c93d05941619e2449837
dantegg <luroufan327@hotmail.com>
2016-10-12 21:02:51 +0800

    update with beautiful

--- testweather.py
+++ testweather.py
@@ -2,6 +2,9 @@
 import string
 import re
 import urllib2
+from bs4 import BeautifulSoup
+
+
 
 class getWeather():
     def __init__(self):
@@ -9,28 +12,36 @@ class getWeather():
         #self.urlpath = 'http://sh.weather.com.cn'
 
     def get_page(self):
-        request = urllib2.Request(self.urlpath)
-        response = urllib2.urlopen(request).read().decode("utf-8")
+        #request = urllib2.Request(self.urlpath)
+        #html_doc = request.read()
+        page = urllib2.urlopen(self.urlpath)
+        html_doc = page.read()
+        #response = urllib2.urlopen(request).read().decode("utf-8")
         #print response
         #print "============"
+        response = BeautifulSoup(html_doc.decode("utf-8"))
+        print 'get response'
         return response
     def find_temp(self,web):
         print 'start find temperature'
         #current_temperature = re.findall(r'<dl><dd>(.*?)</dd></dl>', web, re.S)
-        current_temperature = re.findall(r'<div.*?class="tem">(.*?)</div>', web, re.S)
+        #current_temperature = re.findall(r'<div.*?class="tem">(.*?)</div>', web, re.S)
         #current_temperature = re.findall(r'<div.*?class="tem"><span>(.*?)</span></div>', web, re.S)
-
-        return current_temperature
-    def get_temp(self):
-        finaltemp =[]
-        while True:
-            web = self.get_page()
-            webdom = self.find_temp(web)
-            if len(webdom):
-                finaltemp = webdom
-                break
-        #print finaltemp
-        return finaltemp
+        #web.div['class']='sk'
+        for i in web.find_all('div',class_='tem'):
+            span = i.find_all('span')
+            print span
+        return web.div
+    # def get_temp(self):
+    #     finaltemp =[]
+    #     while True:
+    #         web = self.get_page()
+    #         webdom = self.find_temp(web)
+    #         if len(webdom):
+    #             finaltemp = webdom
+    #             break
+    #     #print finaltemp
+    #     return finaltemp
 
 def main():
     print """
@@ -42,10 +53,13 @@ def main():
         ###############################
     """
     weather_spider = getWeather()
-    getpage = weather_spider.get_temp()
-    print "get page success"
-    for x in getpage:
-        print x.encode('utf-8')
+    getpage = weather_spider.get_page()
+    getdom = weather_spider.find_temp(getpage)
+    print getdom
+    # getpage = weather_spider.get_temp()
+    # print "get page success"
+    # for x in getpage:
+    #     print x.encode('utf-8')
 
 
 
