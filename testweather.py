8f5dd0e08a30f373b03548fcdd65e436c6be9e95
dantegg <luroufan327@hotmail.com>
2016-10-12 17:23:49 +0800

    update

--- testweather.py
+++ testweather.py
@@ -5,21 +5,32 @@ import urllib2
 
 class getWeather():
     def __init__(self):
-        #self.urlpath = 'http://www.weather.com.cn/weather40d/101020100.shtml'
-        self.urlpath = 'http://sh.weather.com.cn'
+        self.urlpath = 'http://www.weather.com.cn/weather1d/101020100.shtml'
+        #self.urlpath = 'http://sh.weather.com.cn'
 
     def get_page(self):
         request = urllib2.Request(self.urlpath)
         response = urllib2.urlopen(request).read().decode("utf-8")
+        #print response
+        #print "============"
         return response
     def find_temp(self,web):
         print 'start find temperature'
-        current_temperature = re.findall(r'<dl><dd>(.*?)</dd></dl>', web, re.S)
-        #current_temperature = re.findall(r'<div.*?class="tem">(.*?)</div>', web, re.S)
-        print "========="
-        print type(current_temperature)
-        print "========="
-        print current_temperature[0].encode('utf-8')
+        #current_temperature = re.findall(r'<dl><dd>(.*?)</dd></dl>', web, re.S)
+        current_temperature = re.findall(r'<div.*?class="tem">(.*?)</div>', web, re.S)
+        #current_temperature = re.findall(r'<div.*?class="tem"><span>(.*?)</span></div>', web, re.S)
+
+        return current_temperature
+    def get_temp(self):
+        finaltemp =[]
+        while True:
+            web = self.get_page()
+            webdom = self.find_temp(web)
+            if len(webdom):
+                finaltemp = webdom
+                break
+        #print finaltemp
+        return finaltemp
 
 def main():
     print """
@@ -31,9 +42,10 @@ def main():
         ###############################
     """
     weather_spider = getWeather()
-    getpage = weather_spider.get_page()
+    getpage = weather_spider.get_temp()
     print "get page success"
-    weather_spider.find_temp(getpage)
+    for x in getpage:
+        print x.encode('utf-8')
 
 
 
